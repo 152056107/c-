575bab38f8ff9f7d9127f08f51968bd1e15d65a8
wangJinKing <1721135713@qq.com>
2017-12-18 13:04:55 +0800

    third commit

--- 小本/WorkspaceDao.cs
+++ 小本/WorkspaceDao.cs
@@ -8,5 +8,33 @@ using MySql.Data.MySqlClient;
 
 namespace 小本
 {
-   
+    class WorkspaceDao
+    {
+        private MySqlConnection conn = DbTools.GetConnection();
+        public void Save(Workspace workspace)
+        {
+            Console.WriteLine(workspace.Name + "  " + workspace.Time.ToString());
+            string sql = "INSERT INTO `workspace` (`name`, `time`, `note_count`) VALUES ('" + workspace.Name + "', '" + workspace.Time + "', '" + workspace.NoteNum + "');";
+            Console.WriteLine(sql);
+            new MySqlCommand(sql, conn).ExecuteNonQuery();
+
+        }
+        public void Delete(int id)
+        {
+            string sql = "delete from `workspace` where `id`='" + id + "';";
+            new MySqlCommand(sql, conn).ExecuteNonQuery();
+        }
+        public void Rename(int id, string name)
+        {
+            string sql = "update `workspace` set `name`='" + name + "' where `id`='" + id + "';";
+            new MySqlCommand(sql, conn).ExecuteNonQuery();
+
+        }
+        public MySqlDataReader FindAll()
+        {
+            string sql = "select * from workspace;";
+            MySqlDataReader reader = new MySqlCommand(sql, conn).ExecuteReader();
+            return reader;
+        }
+    }
 }
